3826345fce1dcadd858053d9d152413feeb0862a
ledzz <ledzz1994@gmail.com>
2024-12-19 17:39:14 +0200

    wip

---
+++ src/2/sweepRounded.ts
@@ -0,0 +1,45 @@
+import { union } from "@jscad/modeling/src/operations/booleans";
+import {
+  extrudeLinear,
+  extrudeRotate,
+} from "@jscad/modeling/src/operations/extrusions";
+import { rotate, translate } from "@jscad/modeling/src/operations/transforms";
+import Geom2 from "@jscad/modeling/src/geometries/geom2/type";
+
+export const sweepRounded = (
+  baseShape: Geom2,
+  size: number,
+  fillet: number,
+) => {
+  const hs = size / 2;
+  const ins = size - fillet * 2;
+  const inhs = ins / 2;
+
+  const walls = [0, 1, 2, 3].map((i) =>
+    rotate(
+      [0, (i * Math.PI) / 2, 0],
+      translate([hs, 0, -inhs], extrudeLinear({ height: ins }, baseShape)),
+    ),
+  );
+
+  const rounded = [0, 1, 2, 3].map((i) =>
+    rotate(
+      [0, (i * Math.PI) / 2, 0],
+      translate(
+        [hs - fillet, 0, -hs + fillet],
+        rotate(
+          [-Math.PI / 2, 0, 0],
+          extrudeRotate(
+            {
+              startAngle: 0,
+              angle: Math.PI / 2,
+            },
+            translate([fillet, 0, 0], baseShape),
+          ),
+        ),
+      ),
+    ),
+  );
+
+  return union(...walls, ...rounded);
+};
--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
-import {useRef, useState} from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import {Canvas, useFrame} from "@react-three/fiber";
-import {Asdf} from "./1/Box.tsx";
-import {OrbitControls} from "@react-three/drei";
-import { Scad } from './2/Scad.tsx';
+import { useState } from "react";
+import "./App.css";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+import { Scad } from "./2/Scad.tsx";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-      <Canvas>
-          <ambientLight intensity={Math.PI / 2} />
-          <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
-          <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
-<OrbitControls/>
-          <Scad/>
-      </Canvas>
-  )
+    <Canvas>
+      <ambientLight intensity={Math.PI / 2} />
+      <spotLight
+        position={[100, 100, 100]}
+        angle={0.15}
+        penumbra={1}
+        decay={0}
+        intensity={Math.PI}
+      />
+      <pointLight position={[-100, -100, -100]} decay={0} intensity={Math.PI} />
+      <OrbitControls />
+      <axesHelper />
+      <Scad />
+    </Canvas>
+  );
 }
 
-export default App
+export default App;
