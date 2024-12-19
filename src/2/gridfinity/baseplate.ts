0bd8436bbbc471760d763a2ac36203115e4ea9f1
ledzz <ledzz1994@gmail.com>
2024-12-19 20:56:08 +0200

    wip

--- src/2/gridfinity/baseplate.ts
+++ src/2/gridfinity/baseplate.ts
@@ -1,30 +1,21 @@
-import Vec2 from "@jscad/modeling/src/maths/vec2/type";
 import { union } from "@jscad/modeling/src/operations/booleans";
 import { rotate, translate } from "@jscad/modeling/src/operations/transforms";
 import { polygon } from "@jscad/modeling/src/primitives";
 import { sweepRounded } from "../sweepRounded.ts";
 import { extrudeLinear } from "@jscad/modeling/src/operations/extrusions";
 import roundedRectangle from "@jscad/modeling/src/primitives/roundedRectangle";
+import { baseHeight, basePoly, baseWidth } from "./constants.ts";
 
 export function baseplate({ size = 42, fillet = 0.8, tolerance = 0.5 } = {}) {
-  const basePoly = [
-    [0, 0], // Innermost bottom point
-    [0.8, 0.8], // Up and out at a 45 degree angle
-    [0.8, 0.8 + 1.8], // Straight up
-    [0.8 + 2.15, 0.8 + 1.8 + 2.15], // Up and out at a 45 degree angle
-    [0, 0.8 + 1.8 + 2.15],
-  ] satisfies Vec2[];
-  const width = Math.max(...basePoly.map((point) => point[0]));
-  const height = Math.max(...basePoly.map((point) => point[1]));
-  const baseSize = size - width * 2 - tolerance;
+  const baseSize = size - baseWidth * 2 - tolerance;
 
   return union(
     translate(
-      [0, height, 0],
+      [0, baseHeight, 0],
       rotate(
         [Math.PI / 2, 0, 0],
         extrudeLinear(
-          { height },
+          { height: baseHeight },
           roundedRectangle({ size: [baseSize, baseSize], roundRadius: fillet }),
         ),
       ),
--- src/2/gridfinity/box.ts
+++ src/2/gridfinity/box.ts
@@ -1,11 +1,18 @@
 import { union } from "@jscad/modeling/src/operations/booleans";
 import { baseplate } from "./baseplate.ts";
-import { translate } from "@jscad/modeling/src/operations/transforms";
+import {
+  rotateX,
+  translate,
+  translateY,
+} from "@jscad/modeling/src/operations/transforms";
 import RecursiveArray from "@jscad/modeling/src/utils/recursiveArray";
 import Geom3 from "@jscad/modeling/src/geometries/geom3/type";
-import { cuboid } from "@jscad/modeling/src/primitives";
+import { extrudeLinear } from "@jscad/modeling/src/operations/extrusions";
+import roundedRectangle from "@jscad/modeling/src/primitives/roundedRectangle";
+import { baseHeight } from "./constants.ts";
 
 export function box({ width = 1, depth = 1, height = 1, size = 42 } = {}) {
+  const outerFillet = 3.25; // TODO: Calculate
   const items: RecursiveArray<Geom3> = [];
 
   for (let i = 0; i < width; i++) {
@@ -20,9 +27,18 @@ export function box({ width = 1, depth = 1, height = 1, size = 42 } = {}) {
   }
   return union(
     ...items,
-    cuboid({
-      center: [0, 10, 0],
-      size: [width * size, height * 7, depth * size],
-    }),
+    translateY(
+      baseHeight,
+      rotateX(
+        -Math.PI / 2,
+        extrudeLinear(
+          { height: height * 7 },
+          roundedRectangle({
+            size: [width * size, depth * size],
+            roundRadius: outerFillet,
+          }),
+        ),
+      ),
+    ),
   );
 }
