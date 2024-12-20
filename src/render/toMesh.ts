1c3aa1b74ac837287222c3c5dacd747dd815627c
ledzz <ledzz1994@gmail.com>
2024-12-20 12:28:59 +0200

    wip

--- src/render/toMesh.ts
+++ src/render/toMesh.ts
@@ -1,35 +1,24 @@
 import Geom3 from "@jscad/modeling/src/geometries/geom3/type";
-import { Geom2 } from "@jscad/modeling/src/geometries/geom2";
-import { Path2 } from "@jscad/modeling/src/geometries/path2";
+import geom2, { Geom2 } from "@jscad/modeling/src/geometries/geom2";
+import path2 from "@jscad/modeling/src/geometries/path2";
 import { create2DLineObject } from "./geom2.ts";
 import { createClosedPolygonObject } from "./path2.ts";
 import { Mesh } from "three";
 import { createComplexGeometry, createComplexMaterial } from "./geom3.ts";
+import { geom3 } from "@jscad/modeling/src/geometries/index";
 
 export function toMesh(geom: Geom3 | Geom2) {
-  if (isGeom2(geom)) {
+  if (geom2.isA(geom)) {
     return create2DLineObject(geom);
   }
-  if (isPath2(geom)) {
+  if (path2.isA(geom)) {
     return createClosedPolygonObject(geom);
   }
-  if (isGeom3(geom)) {
+  if (geom3.isA(geom)) {
     return new Mesh(createComplexGeometry(geom), createComplexMaterial(geom));
   }
 
   console.error("unknown geometry type");
 
   return;
-}
-
-function isGeom3(jsonData: Geom3 | Geom2): jsonData is Geom3 {
-  return (jsonData as Geom3).polygons !== undefined;
-}
-
-function isGeom2(geom: Geom3 | Geom2): geom is Geom2 {
-  return (geom as Geom2).sides !== undefined;
-}
-
-function isPath2(jsonData: Path2 | Geom3 | Geom2): jsonData is Path2 {
-  return (jsonData as Path2).points !== undefined;
 }
