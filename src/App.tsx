a0b30eed38ad7f13466cbbe737c3d5e93cd1c281
ledzz <ledzz1994@gmail.com>
2024-12-20 12:22:04 +0200

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,28 @@ import { Environment, OrbitControls } from "@react-three/drei";
 import { World } from "./app/World.tsx";
 import { BoxEdit } from "./app/BoxEdit.tsx";
 import { useStore } from "./app/store.ts";
+import { useCallback } from "react";
+import { serialize } from "@jscad/stl-serializer";
+import { box } from "./gridfinity/box.ts";
 
 function App() {
-  const box = useStore((state) => state.items[0]);
+  const boxJson = useStore((state) => state.items[0]);
+  const saveStl = useCallback(() => {
+    const rawData = serialize({ binary: true }, box(boxJson));
+    const blob = new Blob(rawData);
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement("a");
+    a.href = url;
+    a.download = "model.stl";
+    a.click();
+  }, [boxJson]);
   return (
     <>
       <BoxEdit
-        box={box}
+        box={boxJson}
         onChange={(box) => useStore.setState({ items: [box] })}
       />
+      <button onClick={saveStl}>export</button>
       <Canvas
         camera={{
           near: 0.1,
