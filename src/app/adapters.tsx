0a06f8f594e0dcd716f935ee0ff274f25a91fce2
ledzz <ledzz1994@gmail.com>
2024-12-22 00:39:01 +0200

    wip

--- src/app/adapters.tsx
+++ src/app/adapters.tsx
@@ -3,16 +3,16 @@ import { box } from "../gridfinity/box.ts";
 import { toMesh } from "../render/toMesh.ts";
 import { baseplate } from "../gridfinity/baseplate.ts";
 
-function create<Props>(
-  render: (
-    props: Props & { onClick: () => void },
-  ) => Parameters<typeof toMesh>[0],
-) {
-  return ({ onClick, ...props }: Props & { onClick: () => void }) => {
+const create =
+  <Props,>(
+    render: (
+      props: Props & { onClick: () => void },
+    ) => Parameters<typeof toMesh>[0],
+  ) =>
+  ({ onClick, ...props }: Props & { onClick: () => void }) => {
     const obj = useMemo(() => toMesh(render(props)), [props]);
     return obj ? <primitive object={obj} onClick={onClick}></primitive> : null;
   };
-}
 
 export const Box = create(box);
 export const Baseplate = create(baseplate);
