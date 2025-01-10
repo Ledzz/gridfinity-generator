579aa21c9eda2751409a8579772de0544bc17860
ledzz <ledzz@mayflower.work>
2025-01-10 17:55:03 +0200

    wip

--- src/manifold/baseplate/baseplate.ts
+++ src/manifold/baseplate/baseplate.ts
@@ -1,11 +1,11 @@
 import { DEFAULT_QUALITY, SIZE } from "../../gridfinity/constants.ts";
-import { ManifoldToplevel } from "manifold-3d";
-import { centerHole } from "./centerHole.ts";
-import { mapReduce2D } from "../../gridfinity/utils/range.ts";
-import { profile } from "./profile.ts";
+import { ManifoldToplevel, Vec3 } from "manifold-3d";
 import { profileBaseHeight, profileBaseWidth } from "./constants.ts";
+import { mapReduce2D } from "../../gridfinity/utils/range.ts";
+import { centerHole } from "./centerHole.ts";
 import { connectorHoles } from "./connectorHole.ts";
 import { magnetHoles } from "./magnetHoles.ts";
+import { profile } from "./profile.ts";
 
 export interface BaseplateGeomProps {
   style: "refined-lite";
@@ -34,73 +34,172 @@ export const baseplate = (
   const { Manifold, CrossSection } = wasm;
   switch (style) {
     case "refined-lite": {
-      return (
-        CrossSection.square([SIZE * width, SIZE * depth], true)
-          // TODO: chamfer
-          .extrude(height + profileBaseHeight)
-          .subtract(
-            Manifold.union(
-              mapReduce2D(width, depth, (x, y) =>
-                centerHole(wasm, {
-                  width,
-                  depth,
-                  height,
-                  x,
-                  y,
-                  hasMagnetHoles,
-                })
-                  // Hollow inside
-                  .add(
-                    CrossSection.square(
-                      [
-                        SIZE - (1.15 + profileBaseWidth) * 2,
-                        SIZE - (1.15 + profileBaseWidth) * 2,
-                      ],
-                      true,
-                    )
-                      .offset(1.15 + profileBaseWidth, "Round")
-                      .extrude(profileBaseHeight)
-                      .translate([0, 0, height]),
-                  )
-                  .add(
-                    hasStackableConnectors
-                      ? connectorHoles(wasm, { width, depth, height, x, y })
-                      : Manifold.union([]),
-                  )
-                  .add(
-                    hasMagnetHoles
-                      ? magnetHoles(wasm, {
-                          baseWidth: profileBaseWidth,
-                          quality,
-                        })
-                      : Manifold.union([]),
-                  )
-                  .translate([
-                    SIZE *
-                      (x -
-                        (width % 2 === 0 ? width / 2 - 0.5 : width / 2 - 0.5)),
-                    SIZE *
-                      (y -
-                        (depth % 2 === 0 ? depth / 2 - 0.5 : depth / 2 - 0.5)),
-                    0,
-                  ]),
-              ),
-            ),
-          )
-          .add(
-            Manifold.union(
-              mapReduce2D(width, depth, (x, y) =>
-                profile(wasm, { quality }).translate([
-                  SIZE *
-                    (x - (width % 2 === 0 ? width / 2 - 0.5 : width / 2 - 0.5)),
-                  SIZE *
-                    (y - (depth % 2 === 0 ? depth / 2 - 0.5 : depth / 2 - 0.5)),
-                  height,
-                ]),
-              ),
-            ),
-          )
+      const p = Manifold.union(
+        mapReduce2D(width, depth, (x, y) =>
+          profile(wasm, { quality }).translate([
+            SIZE * (x - (width % 2 === 0 ? width / 2 - 0.5 : width / 2 - 0.5)),
+            SIZE * (y - (depth % 2 === 0 ? depth / 2 - 0.5 : depth / 2 - 0.5)),
+            height,
+          ]),
+        ),
       );
+      return Manifold.compose([
+        p,
+        Manifold.difference([
+          CrossSection.square([SIZE * width, SIZE * depth], true)
+            // TODO: chamfer
+            .extrude(height + profileBaseHeight),
+          ...mapReduce2D(width, depth, (x, y) => {
+            const translate = [
+              SIZE *
+                (x - (width % 2 === 0 ? width / 2 - 0.5 : width / 2 - 0.5)),
+              SIZE *
+                (y - (depth % 2 === 0 ? depth / 2 - 0.5 : depth / 2 - 0.5)),
+              0,
+            ] as Vec3;
+            return [
+              centerHole(wasm, {
+                width,
+                depth,
+                height,
+                x,
+                y,
+                hasMagnetHoles,
+              })
+                // Hollow inside
+                .add(
+                  CrossSection.square(
+                    [
+                      SIZE - (1.15 + profileBaseWidth) * 2,
+                      SIZE - (1.15 + profileBaseWidth) * 2,
+                    ],
+                    true,
+                  )
+                    .offset(1.15 + profileBaseWidth, "Round")
+                    .extrude(profileBaseHeight)
+                    .translate([0, 0, height]),
+                )
+                .add(
+                  hasStackableConnectors
+                    ? connectorHoles(wasm, { width, depth, height, x, y })
+                    : Manifold.union([]),
+                )
+                .add(
+                  hasMagnetHoles
+                    ? magnetHoles(wasm, {
+                        baseWidth: profileBaseWidth,
+                        quality,
+                      })
+                    : Manifold.union([]),
+                )
+                .translate(translate),
+            ];
+          }).flat(),
+        ]),
+      ]);
     }
   }
+  // Hollow inside
+  // .add(
+  //   CrossSection.square(
+  //     [
+  //       SIZE - (1.15 + profileBaseWidth) * 2,
+  //       SIZE - (1.15 + profileBaseWidth) * 2,
+  //     ],
+  //     true,
+  //   )
+  //     .offset(1.15 + profileBaseWidth, "Round")
+  //     .extrude(profileBaseHeight)
+  //     .translate([0, 0, height]),
+  // )
+  // .add(
+  //   hasStackableConnectors
+  //     ? connectorHoles(wasm, { width, depth, height, x, y })
+  //     : Manifold.union([]),
+  // )
+  // .add(
+  //   hasMagnetHoles
+  //     ? magnetHoles(wasm, {
+  //         baseWidth: profileBaseWidth,
+  //         quality,
+  //       })
+  //     : Manifold.union([]),
+  // )
+  // .translate([
+  //   SIZE *
+  //     (x -
+  //       (width % 2 === 0 ? width / 2 - 0.5 : width / 2 - 0.5)),
+  //   SIZE *
+  //     (y -
+  //       (depth % 2 === 0 ? depth / 2 - 0.5 : depth / 2 - 0.5)),
+  //   0,
+  // ]),
+  // ).flat(),
+  // ),
+  // )
+  // .add(
+  //   Manifold.union(
+  //     mapReduce2D(width, depth, (x, y) =>
+  //       profile(wasm, { quality }).translate([
+  //         SIZE *
+  //           (x - (width % 2 === 0 ? width / 2 - 0.5 : width / 2 - 0.5)),
+  //         SIZE *
+  //           (y - (depth % 2 === 0 ? depth / 2 - 0.5 : depth / 2 - 0.5)),
+  //         height,
+  //       ]),
+  //     ),
+  //   ),
+  // )
+  // );
+  // }
+  // }
 };
+//
+//
+// Manifold.union(
+//     mapReduce2D(width, depth, (x, y) =>
+//         centerHole(wasm, {
+//             width,
+//             depth,
+//             height,
+//             x,
+//             y,
+//             hasMagnetHoles,
+//         })
+//             // Hollow inside
+//             .add(
+//                 CrossSection.square(
+//                     [
+//                         SIZE - (1.15 + profileBaseWidth) * 2,
+//                         SIZE - (1.15 + profileBaseWidth) * 2,
+//                     ],
+//                     true,
+//                 )
+//                     .offset(1.15 + profileBaseWidth, "Round")
+//                     .extrude(profileBaseHeight)
+//                     .translate([0, 0, height]),
+//             )
+//             .add(
+//                 hasStackableConnectors
+//                     ? connectorHoles(wasm, { width, depth, height, x, y })
+//                     : Manifold.union([]),
+//             )
+//             .add(
+//                 hasMagnetHoles
+//                     ? magnetHoles(wasm, {
+//                         baseWidth: profileBaseWidth,
+//                         quality,
+//                     })
+//                     : Manifold.union([]),
+//             )
+//             .translate([
+//                 SIZE *
+//                 (x -
+//                     (width % 2 === 0 ? width / 2 - 0.5 : width / 2 - 0.5)),
+//                 SIZE *
+//                 (y -
+//                     (depth % 2 === 0 ? depth / 2 - 0.5 : depth / 2 - 0.5)),
+//                 0,
+//             ]),
+//     ),
+// ),
